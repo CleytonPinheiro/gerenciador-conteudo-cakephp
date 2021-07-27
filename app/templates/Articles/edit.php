ab785393b8908697f305fc3660bea2beaac5de5c
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-07-27 12:55:55 -0300

    Implementando as tags

--- app/templates/Articles/edit.php
+++ app/templates/Articles/edit.php
@@ -5,6 +5,11 @@
     echo $this->Form->control('user_id', ['type' => 'hidden']);
     echo $this->Form->control('title');
     echo $this->Form->control('body', ['rows' => '3']);
+    // Get a list of tags.
+    $tags = $this->Articles->Tags->find('list')->all();
+
+    // Set tags to the view context
+    $this->set('tags', $tags);
     echo $this->Form->button(__('Save Article'));
     echo $this->Form->end();
 ?>
