0c4ef660ac0c3852b67c4b4ea388fca672351f30
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-07-28 11:18:27 -0300

    Acesso ao view e index sem precisar logar no app

--- app/src/Controller/AppController.php
+++ app/src/Controller/AppController.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Cake\Controller\Controller;
-
 /**
  * Application Controller
  *
@@ -45,11 +44,16 @@ class AppController extends Controller
         $this->loadComponent('Flash');
         
         $this->loadComponent('Authentication.Authentication');
-
         /*
          * Enable the following component for recommended CakePHP form protection settings.
          * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
          */
         //$this->loadComponent('FormProtection');
+    }
+
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);    
+        $this->Authentication->addUnauthenticatedActions(['index', 'view']);
     }
 }
